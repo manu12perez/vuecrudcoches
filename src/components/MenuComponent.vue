fb76b31f731a4135b7be82add1d16e4b3cdd53ef
manu12perez <manu12perez@gmail.com>
2024-11-05 13:00:25 +0100

    sin busqueda

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -41,14 +41,14 @@
           </li> -->
         </ul>
 
-        <form role="search">
+        <!-- <form role="search">
           <input
             class="form-control"
             type="search"
             placeholder="Search"
             aria-label="Search"
           />
-        </form>
+        </form> -->
       </div>
     </div>
   </nav>
